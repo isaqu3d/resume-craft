0dedebe32e21bd9dda2e68cd2745cdd7f9a20fff
Isaque de Sousa <isaque.error404@gmail.com>
2025-03-26 11:37:27 -0300

    feat: add UserDropdown component to DashboardLayout

--- src/app/dashboard/layout.tsx
+++ src/app/dashboard/layout.tsx
@@ -1,5 +1,6 @@
 import Logo from "@/app/assets/logo.svg";
 import { NavItems } from "@/components/pages/nav-items";
+import { UserDropdown } from "@/components/pages/user-dropdown";
 
 export default function DashboardLayout({
   children,
@@ -7,16 +8,22 @@ export default function DashboardLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <div className="w-full h-full overflow-hidden grid grid-cols-[300px,1fr]">
+    <div className="w-full h-screen overflow-hidden grid grid-cols-[300px,1fr]">
       <aside className="w-full h-full flex flex-col items-center border-r border-muted">
         <div className="w-full p-6 border-b border-muted">
           <Logo className="max-w-[100px] mx-auto" />
         </div>
 
         <NavItems />
+
+        <div className="w-full mt-auto border-t border-muted px-3 py-4 flex items-center justify-between gap-2">
+          <UserDropdown />
+        </div>
       </aside>
 
-      <main>{children}</main>
+      <main className="p-6 flex flex-col w-full h-full overflow-auto">
+        {children}
+      </main>
     </div>
   );
 }
