4a65be85790aa7d27019112e7ebeea11bf85a7ac
Isaque de Sousa <isaque.error404@gmail.com>
2025-03-27 19:22:42 -0300

    feat: add AddResumeButton component

---
+++ src/components/pages/dashboard/resumes/add-resume-button.tsx
@@ -0,0 +1,10 @@
+import { Plus } from "lucide-react";
+import { ResumeCardButton } from "./resume-card";
+
+export function AddResumeButton() {
+  <ResumeCardButton
+    title="Criar novo curriculo"
+    description="Comece do zero"
+    icon={<Plus size={50} />}
+  />;
+}
