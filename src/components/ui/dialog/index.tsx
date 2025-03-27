fbc0199bdcc734496475958e1a7024807b498ba3
Isaque de Sousa <isaque.error404@gmail.com>
2025-03-27 19:19:46 -0300

    feat: add Dialog component and its primitive components

---
+++ src/components/ui/dialog/index.tsx
@@ -0,0 +1,44 @@
+import {
+  DialogContent,
+  DialogDescription,
+  DialogHeader,
+  Dialog as DialogRoot,
+  DialogTitle,
+  DialogTrigger,
+} from "./primitive";
+
+type DialogProps = {
+  title: string;
+  description?: string;
+  content: React.ReactNode;
+  open: boolean;
+  setIsOpen: (open: boolean) => void;
+  children: React.ReactNode;
+};
+
+export function Dialog({
+  title,
+  description,
+  content,
+  open,
+  setIsOpen,
+  children,
+}: DialogProps) {
+  return (
+    <>
+      <DialogRoot open={open} onOpenChange={setIsOpen}>
+        {children && <DialogTrigger asChild>{children}</DialogTrigger>}
+        <DialogContent>
+          <DialogHeader>
+            <DialogTitle>{title}</DialogTitle>
+            {description && (
+              <DialogDescription>{description}</DialogDescription>
+            )}
+          </DialogHeader>
+
+          {content}
+        </DialogContent>
+      </DialogRoot>
+    </>
+  );
+}
