593753cf2d462f1d464e6805e8e19590d2f0e24b
Isaque de Sousa <isaque.error404@gmail.com>
2025-03-29 21:42:11 -0300

    feat: add dashboard resume page

---
+++ src/app/dashboard/resumes/page.tsx
@@ -0,0 +1,10 @@
+import { ResumesList } from "@/components/pages/dashboard/resumes/resumes-list";
+
+export default function DashboardResumePages() {
+  return (
+    <>
+      <h1 className="text-black">Curriculo</h1>
+      <ResumesList />
+    </>
+  );
+}
