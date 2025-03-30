3ee8969e5208a1edbae61d4e4ff20c4149195c06
Isaque de Sousa <isaque.error404@gmail.com>
2025-03-30 14:10:10 -0300

    feat: add new resume creation form

--- src/components/pages/dashboard/resumes/new-resume-dialog.tsx
+++ src/components/pages/dashboard/resumes/new-resume-dialog.tsx
@@ -1,12 +1,37 @@
+import { Button } from "@/components/ui/button";
 import { BaseDialogProps, Dialog } from "@/components/ui/dialog";
+import { InputField } from "@/components/ui/input/field";
+import { FormProvider, useForm } from "react-hook-form";
+
+type FormData = {
+  title: string;
+};
 
 export function NewResumeDialog(props: BaseDialogProps) {
+  const methods = useForm<FormData>();
+
+  const onSubmit = (data: FormData) => {
+    console.log(data);
+  };
   return (
     <Dialog
       {...props}
       title="Criar novo curriculo"
       description="Para começar, escolha um título para seu curriculo"
-      content={<form></form>}
+      content={
+        <FormProvider {...methods}>
+          <form
+            className="flex flex-col"
+            onSubmit={methods.handleSubmit(onSubmit)}
+          >
+            <InputField label="Título" name="title" required />
+
+            <Button type="submit" className="w-max mt-6 ml-auto">
+              Criar
+            </Button>
+          </form>
+        </FormProvider>
+      }
     ></Dialog>
   );
 }
