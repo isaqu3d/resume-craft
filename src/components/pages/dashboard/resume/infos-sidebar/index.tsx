04c6ec196c034f2225ffa3f34d0ef1a128e77f0e
Isaque de Sousa <isaque.error404@gmail.com>
2025-04-02 15:57:10 -0300

    feat: add Separator and BasicInfoSection components to InfosSidebar

--- src/components/pages/dashboard/resume/infos-sidebar/index.tsx
+++ src/components/pages/dashboard/resume/infos-sidebar/index.tsx
@@ -1,6 +1,8 @@
 import Logo from "@/app/assets/logo.svg";
+import { Separator } from "@/components/ui/separator";
 import Link from "next/link";
 import { AiGenerationDropdown } from "./ai-generation-dropdown";
+import { BasicInfoSection } from "./sections/basic-info";
 
 export function InfosSidebar() {
   return (
@@ -9,8 +11,13 @@ export function InfosSidebar() {
         <Link href="/dashboard/resumes">
           <Logo className="w-full max-w-[80px]" />
         </Link>
+
         <AiGenerationDropdown />
       </div>
+
+      <Separator className="my-5" />
+
+      <BasicInfoSection />
     </aside>
   );
 }
