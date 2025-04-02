ecce5e912fb0dce6a6b397bd448a391b3b042d4b
Isaque de Sousa <isaque.error404@gmail.com>
2025-04-02 15:21:36 -0300

    chore: add logo and AI generation dropdown to InfosSidebar

--- src/components/pages/dashboard/resume/infos-sidebar/index.tsx
+++ src/components/pages/dashboard/resume/infos-sidebar/index.tsx
@@ -1,3 +1,16 @@
+import Logo from "@/app/assets/logo.svg";
+import Link from "next/link";
+import { AiGenerationDropdown } from "./ai-generation-dropdown";
+
 export function InfosSidebar() {
-  return <aside className="w-full h-full p-6 overflow-y-auto"></aside>;
+  return (
+    <aside className="w-full h-full p-6 overflow-y-auto">
+      <div className="w-full flex items-center justify-between">
+        <Link href="/dashboard/resumes">
+          <Logo className="w-full max-w-[80px]" />
+        </Link>
+        <AiGenerationDropdown />
+      </div>
+    </aside>
+  );
 }
