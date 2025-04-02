6d79bf78bbcc2ad80f8c83d44b8531c1c22ddb18
Isaque de Sousa <isaque.error404@gmail.com>
2025-04-02 15:43:57 -0300

    refactor: refactor resume page layout and add form provider

--- src/components/pages/dashboard/resume/index.tsx
+++ src/components/pages/dashboard/resume/index.tsx
@@ -1,32 +1,39 @@
+"use client";
+
 import {
   ResizableHandle,
   ResizablePanel,
   ResizablePanelGroup,
 } from "@/components/ui/resizable";
+import { FormProvider, useForm } from "react-hook-form";
 import { InfosSidebar } from "./infos-sidebar";
 import { ResumeContent } from "./resume-content";
 import { StructureSidebar } from "./structure-sidebar";
 
 export function ResumePage() {
+  const methods = useForm<ResumeData>();
+
   return (
-    <main className="w-full h-screen overflow-hidden">
-      <ResizablePanelGroup direction="horizontal" className="w-full h-full">
-        <ResizablePanel minSize={20} maxSize={40} defaultSize={30}>
-          <InfosSidebar />
-        </ResizablePanel>
+    <FormProvider {...methods}>
+      <main className="w-full h-screen overflow-hidden">
+        <ResizablePanelGroup direction="horizontal" className="w-full h-full">
+          <ResizablePanel minSize={20} maxSize={40} defaultSize={30}>
+            <InfosSidebar />
+          </ResizablePanel>
 
-        <ResizableHandle withHandle />
+          <ResizableHandle withHandle />
 
-        <ResizablePanel>
-          <ResumeContent />
-        </ResizablePanel>
+          <ResizablePanel>
+            <ResumeContent />
+          </ResizablePanel>
 
-        <ResizableHandle withHandle />
+          <ResizableHandle withHandle />
 
-        <ResizablePanel minSize={20} maxSize={35} defaultSize={25}>
-          <StructureSidebar />
-        </ResizablePanel>
-      </ResizablePanelGroup>
-    </main>
+          <ResizablePanel minSize={20} maxSize={35} defaultSize={25}>
+            <StructureSidebar />
+          </ResizablePanel>
+        </ResizablePanelGroup>
+      </main>
+    </FormProvider>
   );
 }
