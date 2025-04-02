38527441bc98bc3d228fd02025e0f0cdbe49a8a8
Isaque de Sousa <isaque.error404@gmail.com>
2025-04-02 15:56:56 -0300

    feat: add switch field and additional input fields to BasicInfoSection

--- src/components/pages/dashboard/resume/infos-sidebar/sections/basic-info.tsx
+++ src/components/pages/dashboard/resume/infos-sidebar/sections/basic-info.tsx
@@ -1,4 +1,5 @@
 import { InputField } from "@/components/ui/input/field";
+import { SwitchField } from "@/components/ui/switch/field";
 import { UserRound } from "lucide-react";
 import { SectionTitle } from "../section-title";
 
@@ -15,7 +16,16 @@ export function BasicInfoSection() {
             name="content.image.url"
             containerClassName="flex-1"
           />
+
+          <SwitchField name="content.image.visible" className="mb-2" />
         </div>
+
+        <InputField label="Nome completo" name="content.infos.fullName" />
+        <InputField label="Cabeçalho" name="content.infos.headline" />
+        <InputField label="E-mail" name="content.infos.email" />
+        <InputField label="Site" name="content.infos.website" />
+        <InputField label="Telefone" name="content.infos.phone" />
+        <InputField label="Localização" name="content.infos.location" />
       </div>
     </div>
   );
