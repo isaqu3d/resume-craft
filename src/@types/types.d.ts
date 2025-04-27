eaaf64a86cb9bce9d18777336c0efc6752c06126
Isaque de Sousa <isaque.error404@gmail.com>
2025-04-27 12:39:00 -0300

    refactor: refactor types in types.d.ts file

--- src/@types/types.d.ts
+++ src/@types/types.d.ts
@@ -1,23 +1,67 @@
-type ResumeImageData = {
+type ResumeSocialMediaData = {
+  name: string;
+  username: string;
   url: string;
-  visible: boolean;
+  icon: string;
 };
 
-type ResumeInfosData = {
-  fullName: string;
-  headline: string;
-  email: string;
-  website: string;
-  phone: string;
+type ResumeExperienceData = {
+  company: string;
+  position: string;
+  date: string;
   location: string;
+  website: string;
+  summary: string;
+};
+
+type ResumeEducationData = {
+  institution: string;
+  degree: string;
+  location: string;
+  date: string;
+  website: string;
+  summary: string;
+};
+
+type ResumeSkillData = {
+  name: string;
+  description: string;
+  level: number;
+  keywords: string;
+};
+
+type ResumeLanguageData = {
+  name: string;
+  description: string;
+  level: number;
+};
+
+type ResumeCertificationData = {
+  name: string;
+  institution: string;
+  date: string;
+  website: string;
+  summary: string;
+};
+
+type ResumeProjectData = {
+  name: string;
+  description: string;
+  date: string;
+  website: string;
+  summary: string;
+  keywords: string[];
 };
 
 type ResumeContentData = {
   image: ResumeImageData;
   infos: ResumeInfosData;
-};
-
-type ResumeData = {
-  content: ResumeContentData;
-  structure: ResumeStructureData;
+  summary: string;
+  socialMedias: Partial<ResumeSocialMediaData>[];
+  experiences: Partial<ResumeExperienceData>[];
+  educations: Partial<ResumeEducationData>[];
+  skills: Partial<ResumeSkillData>[];
+  languages: Partial<ResumeLanguageData>[];
+  certifications: Partial<ResumeCertificationData>[];
+  projects: Partial<ResumeProjectData>[];
 };
