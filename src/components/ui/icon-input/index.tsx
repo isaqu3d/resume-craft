3fab9cf520bf7aa39c6ef3deea05afbfb772f3be
Isaque de Sousa <isaque.error404@gmail.com>
2025-05-22 12:52:28 -0300

    fix: typo in useDebounce import and update image rendering in IconInput component

--- src/components/ui/icon-input/index.tsx
+++ src/components/ui/icon-input/index.tsx
@@ -1,11 +1,11 @@
-import { useDebounce } from "@/hooks/use-debounce";
-import { Input } from "../input"
+import { useDebounce } from "@/hooks/use-debounces";
+import { Input } from "../input";
 
 type IconInputProps = {
   value: string;
   onChange: (value: string) => void;
   placeholder?: string;
-}
+};
 
 export const IconInput = ({ value, onChange, placeholder }: IconInputProps) => {
   const debouncedValue = useDebounce(value);
@@ -13,13 +13,18 @@ export const IconInput = ({ value, onChange, placeholder }: IconInputProps) => {
   return (
     <div className="flex items-center gap-2">
       <div className="w-8 h-8 min-w-8 rounded-full bg-white p-1.5">
-        {!!debouncedValue && <img src={`https://cdn.simpleicons.org/${debouncedValue}`} className="w-full h-full object-contain" />}
+        {!!debouncedValue && (
+          <img
+            src={`https://cdn.simpleicons.org/${debouncedValue}`}
+            className="w-full h-full object-contain"
+          />
+        )}
       </div>
       <Input
         value={value}
         onChange={(e) => onChange(e.target.value)}
         placeholder={placeholder}
       />
-    </div> 
-  )
-}
+    </div>
+  );
+};
