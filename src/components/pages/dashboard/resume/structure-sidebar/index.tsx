2d8700bedd65a32930f906e016716c55b644edea
Isaque de Sousa <isaque.error404@gmail.com>
2025-05-23 12:48:54 -0300

    chore: add TemplatesListSection to StructureSidebar component.

--- src/components/pages/dashboard/resume/structure-sidebar/index.tsx
+++ src/components/pages/dashboard/resume/structure-sidebar/index.tsx
@@ -1,3 +1,9 @@
+import { TemplatesListSection } from "./sections/templates-list";
+
 export function StructureSidebar() {
-  return <aside className="w-full h-full p-6 overflow-y-auto"></aside>;
+  return (
+    <aside className="w-full h-full p-6 overflow-y-auto">
+      <TemplatesListSection />
+    </aside>
+  );
 }
